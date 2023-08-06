40d5e890c55a78f84df4c59cd7cc07f341b5aac1
Šimon Prokopec <prokopec.simon@nvsp.cz>
2023-08-06 10:46:47 +0200

    Temporary removed Email

--- src/components/SocialLinks.svelte
+++ src/components/SocialLinks.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   //import Discord from "./icons/Discord.svelte";
-  import Email from "./icons/Email.svelte";
+  // import Email from "./icons/Email.svelte";
   import Github from "./icons/Github.svelte";
 </script>
 
@@ -12,6 +12,6 @@
     <!-- <Discord /> -->
   </a>
   <a href="https://github.com/prokopec-simon">
-    <Email />
+    <!-- <Email /> -->
   </a>
 </div>
