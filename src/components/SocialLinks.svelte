49d37232178aa11e3b0dc6d48f8569c1e05f39fd
Šimon Prokopec <prokopec.simon@nvsp.cz>
2023-08-06 10:47:36 +0200

    Removed all icon components

--- src/components/SocialLinks.svelte
+++ src/components/SocialLinks.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
   //import Discord from "./icons/Discord.svelte";
   // import Email from "./icons/Email.svelte";
-  import Github from "./icons/Github.svelte";
+  // import Github from "./icons/Github.svelte";
 </script>
 
 <div class="flex gap-2">
   <a href="https://github.com/prokopec-simon">
-    <Github />
+    <!-- <Github /> -->
   </a>
   <a href="https://github.com/prokopec-simon">
     <!-- <Discord /> -->
